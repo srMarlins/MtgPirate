5dd9d63cd47696c3b4be17c25c56144d8fe65d33
Jared Fowler <jared.fowler@onepeloton.com>
2025-11-09 13:11:02 -0500

    Updating to toml & fixing gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,8 @@
 plugins {
-    kotlin("multiplatform") version "2.2.21"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
-    id("org.jetbrains.compose") version "1.9.3"
-    kotlin("plugin.serialization") version "2.2.21"
-}
-
-// Centralized version declarations (adjust here when updating)
-@Suppress("MemberVisibilityCanBePrivate")
-object Versions {
-    const val serialization = "1.9.0"
-    const val datetime = "0.7.1"
-    const val ksoup = "0.2.5"
-    const val coroutines = "1.9.0"
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "org.srmarlins"
@@ -31,13 +22,12 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.serialization}")
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:${Versions.datetime}")
-                implementation("com.fleeksoft.ksoup:ksoup:${Versions.ksoup}")
+                implementation(libs.kotlinx.serialization.json)
+                implementation(libs.kotlinx.datetime)
+                implementation(libs.ksoup)
                 implementation(compose.runtime)
                 implementation(compose.foundation)
-                // Navigation 3 (AndroidX Navigation Compose repackaged for Multiplatform)
-                implementation("org.jetbrains.androidx.navigation:navigation-compose:2.9.1")
+                implementation(libs.navigation.compose)
             }
         }
         val commonTest by getting {
@@ -50,8 +40,8 @@ kotlin {
                 implementation(compose.desktop.currentOs)
                 implementation(compose.material)
                 implementation(compose.foundation)
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:${Versions.coroutines}")
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kotlinx.coroutines.swing)
             }
         }
         val desktopTest by getting {
