842d092c313b7c5af6a8175a7af24b346743308e
Jared Fowler <jared.fowler@onepeloton.com>
2025-11-17 13:22:18 -0500

    Fixing conflicts

--- src/iosMain/kotlin/database/DatabaseDriverFactory.kt
+++ src/iosMain/kotlin/database/DatabaseDriverFactory.kt
@@ -4,15 +4,8 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import org.srmarlins.mtgpirate.db.MtgPirateDatabase
 
-<<<<<<< HEAD
 actual open class DatabaseDriverFactory {
     actual open fun createDriver(): SqlDriver {
-        // Use native SQLite driver for iOS
-        return NativeSqliteDriver(MtgPirateDatabase.Schema, "pirate.db")
-=======
-actual class DatabaseDriverFactory {
-    actual fun createDriver(): SqlDriver {
         return NativeSqliteDriver(MtgPirateDatabase.Schema, "mtg_pirate.db")
->>>>>>> ec045f3 (Base ios implementation)
     }
 }
